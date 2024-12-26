d5f94fabeeb47f096c2c3b22af08d4527402d167
kewal0285 <kewalk@arecle.net>
2024-12-26 17:25:44 +0530

    changes

---
+++ resources/js/components/Unauthorized.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Unauthorized</h2>
+        <p>You do not have access to this page.</p>
+        <button @click="goBack">Go Back</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        goBack() {
+            this.$router.push({ name: "login" });
+        }
+    }
+};
+</script>
