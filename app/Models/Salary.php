7340bd0a6e08139e2554d1bdb4b130ac41b04537
Piyushy0007 <piyushyadavvns358@gmail.com>
2025-01-09 15:01:02 +0530

    piyush changes

--- app/Models/Salary.php
+++ app/Models/Salary.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories;
 use Illuminate\Database\Eloquent\Model;
 
 class Salary extends Model
 {
-
     protected $table = 'salaries';
+    
     protected $fillable = [
         'employee_id',
         'basic_salary',
@@ -16,15 +15,20 @@ class Salary extends Model
         'net_salary',
         'salary_type',
         'payment_mode',
-        'payment_date'
+        'payment_date',
+        'pf_amount'  // Add pf_amount to fillable fields
     ];
-
-    // protected $appends = ['gross_salary'];
 
     // Gross salary is calculated as the sum of basic salary and net salary
     public function getGrossSalaryAttribute()
     {
         return $this->basic_salary + $this->net_salary;
+    }
+
+    // Calculate salary after deducting PF
+    public function getNetSalaryAfterPfAttribute()
+    {
+        return $this->net_salary - $this->pf_amount;
     }
 
     public function employee()
